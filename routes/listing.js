f07b62d2d833ca3b8d94254d9b3ddb7bf0cbf0a1
Ritik kumar <hrithikkumarr21@gmail.com>
2024-04-23 11:55:48 +0530

    New Commit

--- routes/listing.js
+++ routes/listing.js
@@ -4,11 +4,16 @@ const wrapAsync=require("../utils/wrapAsync.js");
 const Listing=require("../models/listing.js");
 const {isLoggedIn,isOwner,validateListing}=require("../middleware.js");
 const listingController=require("../controllers/listings.js");
+const multer  = require('multer');
+const {storage}=require("../cloudConfig.js");   
+const upload = multer({storage});
 
 router.route("/")
 .get(wrapAsync(listingController.index))
-.post(isLoggedIn,validateListing,wrapAsync(listingController.createListing)
-)
+// .post(isLoggedIn,validateListing,wrapAsync(listingController.createListing))
+.post(upload.single('listing[image]'),(req,res)=>{
+    res.send(req.file);
+})
 
 router.get("/new",isLoggedIn,listingController.renderNewForm);
 
